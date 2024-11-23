6792f07255446ae237ddb7a19354ca59f2c2287c
bahrom <baxrom@email.com>
2024-11-23 18:38:00 +0500

    update

--- src/pages/Works.jsx
+++ src/pages/Works.jsx
@@ -1,3 +1,77 @@
 export default function Works() {
-  return <div></div>;
+  return (
+    <main>
+      <section className="Featured-card container">
+        <h1 className="h1">Works</h1>
+        <div className="Featured-card_department">
+          <img className="Featured-img" src="./images/rasm-1.png" alt="" />
+          <div className="Featured-card_department_box">
+            <h3 className="Featured-card_department_title">
+              Designing Dashboards
+            </h3>
+            <div className="span">
+              <span className="raqam">2020</span>
+              <span className="Dashboard">Dashboard</span>
+            </div>
+            <p className="Featured-card_department_text">
+              Amet minim mollit non deserunt ullamco est sit aliqua dolor do
+              amet sint. Velit officia consequat duis enim velit mollit.
+              Exercitation veniam consequat sunt nostrud amet.
+            </p>
+          </div>
+        </div>
+        <div className="Featured-card_department">
+          <img className="Featured-img" src="./images/rasm-2.png" alt="" />
+          <div className="Featured-card_department_box">
+            <h3 className="Featured-card_department_title">
+              Vibrant Portraits of 2020
+            </h3>
+            <div className="span">
+              <span className="raqam">2018</span>
+              <span className="Dashboard">Illustration</span>
+            </div>
+            <p className="Featured-card_department_text">
+              Amet minim mollit non deserunt ullamco est sit aliqua dolor do
+              amet sint. Velit officia consequat duis enim velit mollit.
+              Exercitation veniam consequat sunt nostrud amet.
+            </p>
+          </div>
+        </div>
+        <div className="Featured-card_department">
+          <img className="Featured-img" src="./images/rasm-3.png" alt="" />
+          <div className="Featured-card_department_box">
+            <h3 className="Featured-card_department_title">
+              36 Days of Malayalam type
+            </h3>
+            <div className="span">
+              <span className="raqam">2018</span>
+              <span className="Dashboard">Typography</span>
+            </div>
+            <p className="Featured-card_department_text">
+              Amet minim mollit non deserunt ullamco est sit aliqua dolor do
+              amet sint. Velit officia consequat duis enim velit mollit.
+              Exercitation veniam consequat sunt nostrud amet.
+            </p>
+          </div>
+        </div>
+        <div className="Featured-card_department">
+          <img className="Featured-img" src="./images/rasm-4.png" alt="" />
+          <div className="Featured-card_department_box">
+            <h3 className="Featured-card_department_title">
+              36 Days of Malayalam type
+            </h3>
+            <div className="span">
+              <span className="raqam">2018</span>
+              <span className="Dashboard">Components, Design</span>
+            </div>
+            <p className="Featured-card_department_text">
+              Amet minim mollit non deserunt ullamco est sit aliqua dolor do
+              amet sint. Velit officia consequat duis enim velit mollit.
+              Exercitation veniam consequat sunt nostrud amet.
+            </p>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
 }
