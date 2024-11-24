5c20250cdb467c97cee759e0cce1ce1c20c9cb4c
bahrom <baxrom@email.com>
2024-11-24 12:03:46 +0500

    footer update

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,65 +1,107 @@
+import { FaFacebookSquare } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+
 export default function Blog() {
   return (
-    <main>
-      <section className="blog container">
-        <h1 className="h1">Blog</h1>
-        <div className="Blog-card_box1">
-          <h3 className="Blog-card_box1_title">UI Interactions of the week</h3>
-          <span className="Blog-card_box1_data">
-            <span> 12 Feb 2019 |</span>
-            <span className="Blog-card_box1_data_span">
-              Express, Handlebars
+    <div>
+      <main>
+        <section className="blog container">
+          <h1 className="h1">Blog</h1>
+          <div className="Blog-card_box1">
+            <h3 className="Blog-card_box1_title">
+              UI Interactions of the week
+            </h3>
+            <span className="Blog-card_box1_data">
+              <span> 12 Feb 2019 |</span>
+              <span className="Blog-card_box1_data_span">
+                Express, Handlebars
+              </span>
             </span>
-          </span>
-          <p className="Blog-card_box1_text">
-            Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
-            sint. Velit officia consequat duis enim velit mollit. Exercitation
-            veniam consequat sunt nostrud amet.
-          </p>
-        </div>
-        <div className="Blog-card_box1">
-          <h3 className="Blog-card_box1_title">UI Interactions of the week</h3>
-          <span className="Blog-card_box1_data">
-            <span> 12 Feb 2019 |</span>
-            <span className="Blog-card_box1_data_span">
-              Express, Handlebars
+            <p className="Blog-card_box1_text">
+              Amet minim mollit non deserunt ullamco est sit aliqua dolor do
+              amet sint. Velit officia consequat duis enim velit mollit.
+              Exercitation veniam consequat sunt nostrud amet.
+            </p>
+          </div>
+          <div className="Blog-card_box1">
+            <h3 className="Blog-card_box1_title">
+              UI Interactions of the week
+            </h3>
+            <span className="Blog-card_box1_data">
+              <span> 12 Feb 2019 |</span>
+              <span className="Blog-card_box1_data_span">
+                Express, Handlebars
+              </span>
             </span>
-          </span>
-          <p className="Blog-card_box1_text">
-            Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
-            sint. Velit officia consequat duis enim velit mollit. Exercitation
-            veniam consequat sunt nostrud amet.
-          </p>
-        </div>
-        <div className="Blog-card_box1">
-          <h3 className="Blog-card_box1_title">UI Interactions of the week</h3>
-          <span className="Blog-card_box1_data">
-            <span> 12 Feb 2019 |</span>
-            <span className="Blog-card_box1_data_span">
-              Express, Handlebars
+            <p className="Blog-card_box1_text">
+              Amet minim mollit non deserunt ullamco est sit aliqua dolor do
+              amet sint. Velit officia consequat duis enim velit mollit.
+              Exercitation veniam consequat sunt nostrud amet.
+            </p>
+          </div>
+          <div className="Blog-card_box1">
+            <h3 className="Blog-card_box1_title">
+              UI Interactions of the week
+            </h3>
+            <span className="Blog-card_box1_data">
+              <span> 12 Feb 2019 |</span>
+              <span className="Blog-card_box1_data_span">
+                Express, Handlebars
+              </span>
             </span>
-          </span>
-          <p className="Blog-card_box1_text">
-            Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
-            sint. Velit officia consequat duis enim velit mollit. Exercitation
-            veniam consequat sunt nostrud amet.
-          </p>
-        </div>
-        <div className="Blog-card_box1">
-          <h3 className="Blog-card_box1_title">UI Interactions of the week</h3>
-          <span className="Blog-card_box1_data">
-            <span> 12 Feb 2019 |</span>
-            <span className="Blog-card_box1_data_span">
-              Express, Handlebars
+            <p className="Blog-card_box1_text">
+              Amet minim mollit non deserunt ullamco est sit aliqua dolor do
+              amet sint. Velit officia consequat duis enim velit mollit.
+              Exercitation veniam consequat sunt nostrud amet.
+            </p>
+          </div>
+          <div className="Blog-card_box1">
+            <h3 className="Blog-card_box1_title">
+              UI Interactions of the week
+            </h3>
+            <span className="Blog-card_box1_data">
+              <span> 12 Feb 2019 |</span>
+              <span className="Blog-card_box1_data_span">
+                Express, Handlebars
+              </span>
             </span>
-          </span>
-          <p className="Blog-card_box1_text">
-            Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
-            sint. Velit officia consequat duis enim velit mollit. Exercitation
-            veniam consequat sunt nostrud amet.
-          </p>
+            <p className="Blog-card_box1_text">
+              Amet minim mollit non deserunt ullamco est sit aliqua dolor do
+              amet sint. Velit officia consequat duis enim velit mollit.
+              Exercitation veniam consequat sunt nostrud amet.
+            </p>
+          </div>
+        </section>
+      </main>
+      <footer>
+        <div className="footer container">
+          <ul className="footer-menu">
+            <li>
+              <a className="icon" href="https://www.facebook.com/">
+                <FaFacebookSquare />
+              </a>
+            </li>
+            <li>
+              <a className="icon" href="https://www.instagram.com/">
+                <FaInstagram />
+              </a>
+            </li>
+            <li>
+              <a className="icon" href="https://x.com/">
+                <FaTwitter />
+              </a>
+            </li>
+            <li>
+              <a className="icon" href="https://www.linkedin.com/">
+                <FaLinkedin />
+              </a>
+            </li>
+          </ul>
+          <p className="footer-text">Copyright ©2020 All rights reserved </p>
         </div>
-      </section>
-    </main>
+      </footer>
+    </div>
   );
 }
--- src/pages/Works.jsx
+++ src/pages/Works.jsx
@@ -1,77 +1,110 @@
+import { FaFacebookSquare } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
 export default function Works() {
   return (
-    <main>
-      <section className="Featured-card container">
-        <h1 className="h1">Works</h1>
-        <div className="Featured-card_department">
-          <img className="Featured-img" src="./images/rasm-1.png" alt="" />
-          <div className="Featured-card_department_box">
-            <h3 className="Featured-card_department_title">
-              Designing Dashboards
-            </h3>
-            <div className="span">
-              <span className="raqam">2020</span>
-              <span className="Dashboard">Dashboard</span>
+    <div>
+      <main>
+        <section className="Featured-card container">
+          <h1 className="h1">Works</h1>
+          <div className="Featured-card_department">
+            <img className="Featured-img" src="./images/rasm-1.png" alt="" />
+            <div className="Featured-card_department_box">
+              <h3 className="Featured-card_department_title">
+                Designing Dashboards
+              </h3>
+              <div className="span">
+                <span className="raqam">2020</span>
+                <span className="Dashboard">Dashboard</span>
+              </div>
+              <p className="Featured-card_department_text">
+                Amet minim mollit non deserunt ullamco est sit aliqua dolor do
+                amet sint. Velit officia consequat duis enim velit mollit.
+                Exercitation veniam consequat sunt nostrud amet.
+              </p>
             </div>
-            <p className="Featured-card_department_text">
-              Amet minim mollit non deserunt ullamco est sit aliqua dolor do
-              amet sint. Velit officia consequat duis enim velit mollit.
-              Exercitation veniam consequat sunt nostrud amet.
-            </p>
           </div>
-        </div>
-        <div className="Featured-card_department">
-          <img className="Featured-img" src="./images/rasm-2.png" alt="" />
-          <div className="Featured-card_department_box">
-            <h3 className="Featured-card_department_title">
-              Vibrant Portraits of 2020
-            </h3>
-            <div className="span">
-              <span className="raqam">2018</span>
-              <span className="Dashboard">Illustration</span>
+          <div className="Featured-card_department">
+            <img className="Featured-img" src="./images/rasm-2.png" alt="" />
+            <div className="Featured-card_department_box">
+              <h3 className="Featured-card_department_title">
+                Vibrant Portraits of 2020
+              </h3>
+              <div className="span">
+                <span className="raqam">2018</span>
+                <span className="Dashboard">Illustration</span>
+              </div>
+              <p className="Featured-card_department_text">
+                Amet minim mollit non deserunt ullamco est sit aliqua dolor do
+                amet sint. Velit officia consequat duis enim velit mollit.
+                Exercitation veniam consequat sunt nostrud amet.
+              </p>
             </div>
-            <p className="Featured-card_department_text">
-              Amet minim mollit non deserunt ullamco est sit aliqua dolor do
-              amet sint. Velit officia consequat duis enim velit mollit.
-              Exercitation veniam consequat sunt nostrud amet.
-            </p>
           </div>
-        </div>
-        <div className="Featured-card_department">
-          <img className="Featured-img" src="./images/rasm-3.png" alt="" />
-          <div className="Featured-card_department_box">
-            <h3 className="Featured-card_department_title">
-              36 Days of Malayalam type
-            </h3>
-            <div className="span">
-              <span className="raqam">2018</span>
-              <span className="Dashboard">Typography</span>
+          <div className="Featured-card_department">
+            <img className="Featured-img" src="./images/rasm-3.png" alt="" />
+            <div className="Featured-card_department_box">
+              <h3 className="Featured-card_department_title">
+                36 Days of Malayalam type
+              </h3>
+              <div className="span">
+                <span className="raqam">2018</span>
+                <span className="Dashboard">Typography</span>
+              </div>
+              <p className="Featured-card_department_text">
+                Amet minim mollit non deserunt ullamco est sit aliqua dolor do
+                amet sint. Velit officia consequat duis enim velit mollit.
+                Exercitation veniam consequat sunt nostrud amet.
+              </p>
             </div>
-            <p className="Featured-card_department_text">
-              Amet minim mollit non deserunt ullamco est sit aliqua dolor do
-              amet sint. Velit officia consequat duis enim velit mollit.
-              Exercitation veniam consequat sunt nostrud amet.
-            </p>
           </div>
-        </div>
-        <div className="Featured-card_department">
-          <img className="Featured-img" src="./images/rasm-4.png" alt="" />
-          <div className="Featured-card_department_box">
-            <h3 className="Featured-card_department_title">
-              36 Days of Malayalam type
-            </h3>
-            <div className="span">
-              <span className="raqam">2018</span>
-              <span className="Dashboard">Components, Design</span>
+          <div className="Featured-card_department">
+            <img className="Featured-img" src="./images/rasm-4.png" alt="" />
+            <div className="Featured-card_department_box">
+              <h3 className="Featured-card_department_title">
+                36 Days of Malayalam type
+              </h3>
+              <div className="span">
+                <span className="raqam">2018</span>
+                <span className="Dashboard">Components, Design</span>
+              </div>
+              <p className="Featured-card_department_text">
+                Amet minim mollit non deserunt ullamco est sit aliqua dolor do
+                amet sint. Velit officia consequat duis enim velit mollit.
+                Exercitation veniam consequat sunt nostrud amet.
+              </p>
             </div>
-            <p className="Featured-card_department_text">
-              Amet minim mollit non deserunt ullamco est sit aliqua dolor do
-              amet sint. Velit officia consequat duis enim velit mollit.
-              Exercitation veniam consequat sunt nostrud amet.
-            </p>
           </div>
+        </section>
+      </main>
+      <footer>
+        <div className="footer container">
+          <ul className="footer-menu">
+            <li>
+              <a className="icon" href="https://www.facebook.com/">
+                <FaFacebookSquare />
+              </a>
+            </li>
+            <li>
+              <a className="icon" href="https://www.instagram.com/">
+                <FaInstagram />
+              </a>
+            </li>
+            <li>
+              <a className="icon" href="https://x.com/">
+                <FaTwitter />
+              </a>
+            </li>
+            <li>
+              <a className="icon" href="https://www.linkedin.com/">
+                <FaLinkedin />
+              </a>
+            </li>
+          </ul>
+          <p className="footer-text">Copyright ©2020 All rights reserved </p>
         </div>
-      </section>
-    </main>
+      </footer>
+    </div>
   );
 }
